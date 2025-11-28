0cc73a42e8db53a5097caafa90df188fb23039bc
DinhThi <thi2842005@gmail.com>
2025-11-28 15:15:10 +0700

    lan 2

---
+++ src/components/Mycomponent.js
@@ -0,0 +1,17 @@
+// class component
+// functional component
+
+import React from "react";
+class Mycomponent extends React.Component {
+  // JSX
+  render() {
+    return (
+      <div>
+        <h1>Tôi yêu em </h1>
+        {Math.random()}
+      </div>
+    );
+  }
+}
+// xu
+export default Mycomponent;
